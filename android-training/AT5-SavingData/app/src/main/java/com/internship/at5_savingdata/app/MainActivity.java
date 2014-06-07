99da461dbec150d1a6b0e1dbd42a33df638433eb
ldurazo <ldurazo@nearsoft.com>
2014-06-06 19:07:43 -0700

    write a simple file

--- android-training/AT5-SavingData/app/src/main/java/com/internship/at5_savingdata/app/MainActivity.java
+++ android-training/AT5-SavingData/app/src/main/java/com/internship/at5_savingdata/app/MainActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class MainActivity extends Activity {
     TextView name;
     TextView alias;
     final static String Name="nameKey";
     final static String Alias="aliasKey";
+    File file = new File(this.getFilesDir(), "filename");
+    FileOutputStream outputStream;
     SharedPreferences sharedpreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +46,13 @@ public class MainActivity extends Activity {
         editor.putString(Name, n);
         editor.putString(Alias, a);
         editor.commit();
+
+        try {
+            outputStream = openFileOutput("filename", Context.MODE_PRIVATE);
+            outputStream.write("esto se guardo por ahi en el celular".getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
