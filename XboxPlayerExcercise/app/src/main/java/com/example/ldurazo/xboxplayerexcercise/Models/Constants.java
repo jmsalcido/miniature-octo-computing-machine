88aa3603aa7f1a4b1d8f290417257589d0d050d3
Luis Alberto Durazo Arvizu <ldurazo@nearsoft.com>
2014-07-17 16:14:21 -0700

    Added some constants: client_id, client_secret, callback_url.

--- XboxPlayerExcercise/app/src/main/java/com/example/ldurazo/xboxplayerexcercise/Models/Constants.java
+++ XboxPlayerExcercise/app/src/main/java/com/example/ldurazo/xboxplayerexcercise/Models/Constants.java
@@ -5,9 +5,10 @@ package com.example.ldurazo.xboxplayerexcercise.Models;
  */
 public class Constants {
     public static final String TAG="MUSIC_PLAYER_APP";
-    public static final String CLIENT_ID=null;
-    public static final String CLIENT_SECRET=null;
-    public static final String SERVICE=null;
-    public static final String SCOPE=null;
+    public static final String CLIENT_ID="musicplayer_internship_ldurazo";
+    public static final String CLIENT_SECRET="LDKFAP345sdklfj542564654";
+    public static final String CALLBACK_URL="http://luisdurazoa.tumblr.com/";
+    public static final String SERVICE="https://datamarket.accesscontrol.windows.net/v2/OAuth2-13";
+    public static final String SCOPE="http://music.xboxlive.com";
     public static final String TOKEN="TOKEN";
 }
