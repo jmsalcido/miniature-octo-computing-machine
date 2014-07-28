103f793688fd2701d8caa9d63c4fa2ad9445d6cd
Luis Alberto Durazo Arvizu <ldurazo@nearsoft.com>
2014-07-28 12:10:54 -0700

    Added ImageLoader and Volley libraries.

---
+++ XboxPlayerExcercise/app/src/main/java/com/example/ldurazo/xboxplayerexcercise/applications/ImageLoaderApp.java
@@ -0,0 +1,26 @@
+package com.example.ldurazo.xboxplayerexcercise.applications;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+
+public class ImageLoaderApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Create ImageLoaderConfiguration
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+                .diskCacheFileCount(150)
+                .diskCacheExtraOptions(800, 480, null)
+                .writeDebugLogs()
+                .build();
+
+        //Initialize ImageLoader with configuration
+        ImageLoader.getInstance().init(config);
+    }
+
+}
